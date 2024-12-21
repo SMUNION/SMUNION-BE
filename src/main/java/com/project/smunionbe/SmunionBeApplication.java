8beee9fd717f6d15d50a503ddae479eddb863231
Jimyeong Kim <kjm021221@icloud.com>
2024-12-21 21:50:59 +0900

    first commit

---
+++ src/main/java/com/project/smunionbe/SmunionBeApplication.java
@@ -0,0 +1,13 @@
+package com.project.smunionbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmunionBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmunionBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/smunionbe/SmunionBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.smunionbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmunionBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
