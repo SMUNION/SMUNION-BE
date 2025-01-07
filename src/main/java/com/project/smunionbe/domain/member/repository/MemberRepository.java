92d6bb0a23edf9bd4f7e6eef000b0401a2597fc4
Ohwooseok <dhdntjr369@naver.com>
2025-01-07 20:47:14 +0900

    ✨ feat: 동아리, 갤러리 API 개발(충돌 수정4)

--- src/main/java/com/project/smunionbe/domain/member/repository/MemberRepository.java
+++ src/main/java/com/project/smunionbe/domain/member/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -16,6 +17,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
             "    WHERE a.attendanceNotice.id = :attendanceId AND a.isPresent = true" +
             ")")
     List<Member> findAbsenteesByAttendanceId(@Param("attendanceId") Long attendanceId, @Param("clubId") Long clubId);
+
+    Optional<Member> findByEmail(String email); //이메일로 사용자의 정보를 가져옴
 
     Boolean existsByEmail(String email);
 }
